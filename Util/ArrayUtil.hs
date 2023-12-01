b5056407d0d65c0e5cc71e9e003ee8ecc6c46662
Max Petersen <maxvonpetersen@gmail.com>
2023-12-01 16:44:18 -0700

    added doc comments to ArrayUtil.hs

--- Util/ArrayUtil.hs
+++ Util/ArrayUtil.hs
@@ -17,12 +17,18 @@ collectGroups x y =
         rest = removeFirst $ dropWhile (/= y) x -- removeFirst is removing the separator here
     in upTo : collectGroups rest y
 
+-- | This function collects a list into groups with a given size.
+--   It takes two arguments, the group size and the list, respectively.
+--   If the list can't be grouped evenly into groups of the given size, the last group will be smaller than the given size.
 groupEvery :: Int -> [a] -> [[a]]
 groupEvery _ [] = []
 groupEvery n x
     | n > 0 = take n x : groupEvery n (drop n x)
     | otherwise = error "Can't call groupEvery with negative number"
 
+-- | This function finds the start index of a pattern in a list.
+--   It takes two arguments, the pattern and the array, respectively.
+--   If the pattern does not exist in the list, the function will return -1.
 indexOf :: (Eq a) => [a] -> [a] -> Int
 indexOf [] _ = -1
 indexOf _ [] = -1
@@ -32,12 +38,17 @@ indexOf x y
     | otherwise = let index = indexOf x (tail y)
     in if index == -1 then -1 else index + 1
 
+-- | This function finds the earliest index of each pattern for a list of given patterns.
+--   It takes two arguments, the list of patterns and the list to search through, respectively.
+--   It returns the patterns and their indices as tuples in a list, and if a pattern does not exist it has an index of -1.
 indicesL :: (Eq a) => [[a]] -> [a] -> [(Int, [a])]
 indicesL patterns x = filter (\x -> fst x >= 0) $ map (\p -> (indexOf p x, p)) patterns
 
+-- | This function is essentially an alias for indicesR, except that it searches right to left.
 indicesR :: (Eq a) => [[a]] -> [a] -> [(Int, [a])]
 indicesR patterns x = filter (\x -> fst x >= 0) $ map (\p -> (indexOf (reverse p) $ reverse x, p)) patterns
 
+-- | This function takes an array of orderable elements and sorts it smallest to largest.
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort arr =
@@ -46,6 +57,7 @@ quicksort arr =
         bigger = quicksort [a | a <- xs, a > x]
     in smaller ++ [x] ++ bigger
 
+-- | This function takes an array of any elements and a function to order them, and sorts them smallest to largest.
 quicksort' :: (a -> a -> Ordering) -> [a] -> [a]
 quicksort' _ [] = []
 quicksort' cmp arr = 
