5ee193b4ca829703bd09799d0b5bab4923fbf3a4
Max Petersen <maxvonpetersen@gmail.com>
2023-12-11 10:48:48 -0700

    added placeholder days up to 11

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,11 @@ import qualified Day3 as D3 (part1, part2)
 import qualified Day4 as D4 (part1, part2)
 import qualified Day5 as D5 (part1, part2)
 import qualified Day6 as D6 (part1, part2)
+import qualified Day7 as D7 (part1, part2)
+import qualified Day8 as D8 (part1, part2)
+import qualified Day9 as D9 (part1, part2)
+import qualified Day10 as D10 (part1, part2)
+import qualified Day11 as D11 (part1, part2)
 
 type DayFunction = String -> IO Int
 
@@ -49,4 +54,9 @@ days = DM.fromList [
     (3, [D3.part1, D3.part2]),
     (4, [D4.part1, D4.part2]),
     (5, [D5.part1, D5.part2]),
-    (6, [D6.part1, D6.part2])]
+    (6, [D6.part1, D6.part2]),
+    (7, [D7.part1, D7.part2]),
+    (8, [D8.part1, D8.part2]),
+    (9, [D9.part1, D9.part2]),
+    (10, [D10.part1, D10.part2]),
+    (11, [D11.part1, D11.part2])]
---
+++ src/Day8.hs
@@ -0,0 +1,7 @@
+module Day8 (part1, part2) where
+
+part1 :: String -> IO Int
+part1 input = return 69
+
+part2 :: String -> IO Int
+part2 input = return 420
