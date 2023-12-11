af697c3c7a7cd256d975cbba176f2be1ca775ca6
Max Petersen <maxvonpetersen@gmail.com>
2023-12-11 12:58:38 -0700

    finished day 9

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,7 +1,56 @@
 module Day9 (part1, part2) where
+import Common.ParseUtil
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer
+import Data.Void
 
 part1 :: String -> IO Int
-part1 input = return 69
+part1 input = do
+    let sequences = parseInput input
+    let answer = sum $ map extrapolate sequences
+    return answer
 
 part2 :: String -> IO Int
-part2 input = return 420
+part2 input = do
+    let sequences = parseInput input
+    print (allDerivatives $ head sequences)
+    let answer = sum $ map extrapolateBack sequences
+    return answer
+
+-- Part 1
+
+derive :: [Int] -> [Int]
+derive [x] = []
+derive (x:y:xs) = (y - x) : derive (y:xs)
+
+allDerivatives :: [Int] -> [[Int]]
+allDerivatives xs
+    | all (==0) xs = [xs]
+    | otherwise = xs : allDerivatives (derive xs)
+
+integrate :: [[Int]] -> Int
+integrate = sum . map last
+
+extrapolate :: [Int] -> Int
+extrapolate = integrate . allDerivatives
+
+-- Part 2
+
+integrateBack :: [[Int]] -> Int
+integrateBack ders =
+    let alternatingSigns = cycle [1, -1]
+        first = head (head ders)
+        rest = map head (tail ders)
+        signedRest = zipWith (*) alternatingSigns rest
+    in first - sum signedRest
+
+extrapolateBack :: [Int] -> Int
+extrapolateBack = integrateBack . allDerivatives
+
+-- Parsers
+
+parseInput :: String -> [[Int]]
+parseInput s =
+    let digits = map words (lines s)
+    in map (map read) digits
